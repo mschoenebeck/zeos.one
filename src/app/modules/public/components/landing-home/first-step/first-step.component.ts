e2011821f5ad50cf2a44fb8cb580221a0f84ab1e
Diego R <dieguuuds@gmail.com>
2022-04-28 07:47:38 +0200

    feat(implementation of images): second feature container images

--- src/app/modules/public/components/landing-home/first-step/first-step.component.ts
+++ src/app/modules/public/components/landing-home/first-step/first-step.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { LandingPageAction } from 'src/app/modules/shared/navigation-bar/enums/landing-page-action.enum';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-first-step',
   templateUrl: './first-step.component.html',
   styleUrls: ['./first-step.component.scss']
 })
-export class FirstStepComponent implements OnInit {
+export class FirstStepComponent {
+
+  @Output() landingPageAction = new EventEmitter<LandingPageAction>();
+  public LandingPageAction = LandingPageAction;
 
   constructor() { }
 
-  ngOnInit() {
+  /**
+   * Receives action and emits event, if action doesn't exists returns default FIRST_CONTAINER
+   *
+   * @param landingPageAction LandingPageAction enum
+   */
+  public changeAction(landingPageAction: LandingPageAction) {
+    this.landingPageAction.emit(landingPageAction);
   }
-
 }
--- src/app/modules/shared/navigation-bar/navigation-bar.module.ts
+++ src/app/modules/shared/navigation-bar/navigation-bar.module.ts
@@ -1,7 +1,7 @@
+import { NavigationBarComponent } from './components/navigation-bar.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationBarComponent } from './navigation-bar.component';
 import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
