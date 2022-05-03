c40c58ce532de951a2f5741c0f12bf14524a5721
Diego R <dieguuuds@gmail.com>
2022-05-03 23:37:44 +0200

    feat(finished some of the feature containers and user experience through the whole web page)

--- src/app/modules/public/components/landing-home/fourth-step/fourth-step.component.ts
+++ src/app/modules/public/components/landing-home/fourth-step/fourth-step.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { LandingPageAction } from './../../../../shared/navigation-bar/enums/landing-page-action.enum';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-fourth-step',
   templateUrl: './fourth-step.component.html',
   styleUrls: ['./fourth-step.component.scss']
 })
-export class FourthStepComponent implements OnInit {
+export class FourthStepComponent {
+
+  @Output() landingPageAction = new EventEmitter<LandingPageAction>();
 
   constructor() { }
 
-  ngOnInit() {
+  /**
+   * Receives action and emits event, if action doesn't exists returns default FIRST_CONTAINER
+   *
+   * @param landingPageAction LandingPageAction enum
+   */
+  public changeAction() {
+    this.landingPageAction.emit(LandingPageAction.FIFTH_CONTAINER);
   }
-
 }
--- src/app/modules/public/components/landing-home/third-step/third-step.component.ts
+++ src/app/modules/public/components/landing-home/third-step/third-step.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { LandingPageAction } from './../../../../shared/navigation-bar/enums/landing-page-action.enum';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-third-step',
   templateUrl: './third-step.component.html',
   styleUrls: ['./third-step.component.scss']
 })
-export class ThirdStepComponent implements OnInit {
+export class ThirdStepComponent {
+
+  @Output() landingPageAction = new EventEmitter<LandingPageAction>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  /**
+   * Receives action and emits event, if action doesn't exists returns default FIRST_CONTAINER
+   *
+   * @param landingPageAction LandingPageAction enum
+   */
+  public changeAction() {
+    this.landingPageAction.emit(LandingPageAction.FOURTH_CONTAINER);
   }
-
 }
