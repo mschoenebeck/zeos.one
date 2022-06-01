92bdb99f813cb3e99a6ecb690aeb375365767f7f
Diego R <dieguuuds@gmail.com>
2022-06-01 21:24:15 +0200

    chore(eslintrc.js update): updated configs for eslints

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,20 @@ module.exports = {
   ],
   'rules': {
     'linebreak-style': 0,
+    'require-jsdoc': 0,
+    'object-curly-spacing': 0,
+    'eslint-disable-next-line': 0,
+    'valid-jsdoc': 0,
+    'new-cap': 0,
   },
   'overrides': [
     {
       'files': ['src/**'],
       'rules': {
         'quotes': [2, 'single'],
+        'max-len': [2, { code: 140, ignorePattern: '^import .*' }],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
       },
     },
   ],
