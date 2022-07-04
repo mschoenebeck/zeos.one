435c3f2a39d5c78ea34be3e8d35a69ba74d1d9e7
zerofone <dieguuuds@gmail.com>
2022-07-04 20:40:19 +0200

    feat(added google analytics): added google analytics through all the website, including internal redirections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     PrivateModule,
     PublicModule,
+    NgxGoogleAnalyticsModule.forRoot('G-RE4WSQGJTE'),
+    NgxGoogleAnalyticsRouterModule,
     HttpClientModule,
   ],
   providers: [
