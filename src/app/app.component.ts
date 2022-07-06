f113cf92fe12ca92d7865cce2950d64494d597ae
zerofone <dieguuuds@gmail.com>
2022-07-06 20:21:22 +0200

    fix(ga): google analytics fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare const gtag: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zeosone';
+
+  constructor(private router: Router) {
+    const navEndEvent$ = router.events.pipe(
+        filter((e) => e instanceof NavigationEnd),
+    );
+    navEndEvent$.subscribe((e) => {
+      gtag('config', 'G-3HEYMKXXSC', { 'page_path': (<NavigationEnd>e).urlAfterRedirects });
+    });
+  }
 }
